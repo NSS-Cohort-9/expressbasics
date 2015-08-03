743f1913d0d66eb6b6dd0a1b2fb5bf26af03aa92
Scott Humphries <scott@sscotth.io>
2015-08-03 14:50:08 -0500

    extract routes to own file

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
+
+var routes = require('./routes/index');
+var pizza = require('./routes/pizza');
+
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -8,59 +12,10 @@ app.use(function (req, res, next) {
   console.log('Request at ' + new Date().toISOString());
   next();
 });
-
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/pizza/:topping/:qty', function (req, res) {
-  var obj = req.params;
-  obj.title = 'Pizza Shop';
-
-  res.render('templates/pizza', obj);
-});
-
-app.get(/hello/, function (req, res) {
-  res.send('Hello!');
-});
-
-app.get('/awesomethings', function (req, res) {
-  setTimeout(function () {
-    var awesomeThings = [
-      'Pizza',
-      'Bacon',
-      '2nd Ammendment',
-      'Pluto',
-      'Space Jam'
-    ];
-
-    res.render('templates/world',
-      { title: 'Awesomesite.com',
-        welcome: 'Thanks for coming!',
-        awesomeThings: awesomeThings
-      }
-    );
-  }, 5000);
-});
-
-app.get('/test', function (req, res, next) {
-  res.write('Test1!');
-  next();
-});
-
-app.get('/test', function (req, res) {
-  res.end('Test2!');
-});
-
-app.get('/json', function (req, res) {
-  res.send({an: 'object'});
-});
-
-app.get('/thisshoulderror', function (req, res) {
-  res.send(badVariable);
-});
+app.use('/', routes);
+app.use('/pizza', pizza);
 
 app.use(function (req, res) {
   res.status(403).send('Unauthorized!');
