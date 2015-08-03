b62f062c5a24a67f4fdedcfc9abacffb9b969e6d
Scott Humphries <scott@sscotth.io>
2015-08-03 10:22:12 -0500

    basic routing

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+app.post('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get(/hello/, function (req, res) {
+  res.send('Hello!');
+});
+
+app.get('/world', function (req, res) {
+  res.send('World!');
+});
+
+app.get('/test', function (req, res) {
+  res.send('Test1!');
+});
+
+app.get('/test', function (req, res) {
+  res.send('Test2!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%d', host, port);
+});
