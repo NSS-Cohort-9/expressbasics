ffe59603f1864e1ec6b2b06000beabddd17f464b
Scott Humphries <scott@sscotth.io>
2015-08-03 14:27:19 -0500

    dynamic routes

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/pizza/:topping/:qty', function (req, res) {
+  var obj = req.params;
+  obj.title = 'Pizza Shop';
+
+  res.render('templates/pizza', obj);
+});
+
 app.get(/hello/, function (req, res) {
   res.send('Hello!');
 });
