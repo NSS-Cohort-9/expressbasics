7915ee2399e6f4e665c2d0c24c18ed792241251e
Scott Humphries <scott@sscotth.io>
2015-08-04 09:47:51 -0500

    morgan logging

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+var fs = require('fs');
+
 var express = require('express');
 var lessCSS = require('less-middleware');
+var morgan = require('morgan');
 
 var routes = require('./routes/index');
 var pizza = require('./routes/pizza');
@@ -13,11 +16,10 @@ app.locals.title = 'aweso.me';
 
 app.use(lessCSS('public'));
 
-app.use(function (req, res, next) {
-  // logging at the top
-  console.log('Request at ' + new Date().toISOString());
-  next();
-});
+var logStream = fs.createWriteStream('access.log', {flags: 'a'});
+app.use(morgan('combined', {stream: logStream}));
+app.use(morgan('dev'));
+
 app.use(express.static('public'));
 
 app.use('/', routes);
