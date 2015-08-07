11765a5d7a9be3d3fd127982bffcb158a947241f
Scott Humphries <scott@sscotth.io>
2015-08-07 11:10:36 -0500

    staring chicken order flow

--- routes/chickennuggets.js
+++ routes/chickennuggets.js
@@ -1,15 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+
 router.get('/', function (req, res) {
-  res.render('templates/chickennuggets');
+  var collection = global.db.collection('chickenNuggets');
+
+  collection.find().toArray(function (err, orders) {
+    var formattedOrders = orders.map(function (order) {
+      return {
+        name:      order.name,
+        flavor:    order.style,
+        qty:       order.qty,
+        createdAt: order._id.getTimestamp()
+      };
+    });
+
+    res.render('templates/chicken-index', {orders: formattedOrders});
+  });
+});
+
+router.get('/order', function (req, res) {
+  res.render('templates/chicken-new');
 });
 
 router.post('/order', function (req, res) {
   var collection = global.db.collection('chickenNuggets');
 
   collection.save(req.body, function () {
-    res.redirect('/')
+    res.redirect('/chickennuggets')
   });
 
 });
