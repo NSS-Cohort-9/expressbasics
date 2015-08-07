9d975ad415f6c42e85f31d1b9b34391ca2505d8c
Scott Humphries <scott@sscotth.io>
2015-08-07 13:31:10 -0500

    completed chicken order

--- routes/chickennuggets.js
+++ routes/chickennuggets.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var moment = require('moment');
+var ObjectID = require('mongodb').ObjectID;
 
 var router = express.Router();
 
@@ -9,6 +10,7 @@ router.get('/', function (req, res) {
   collection.find().toArray(function (err, orders) {
     var formattedOrders = orders.map(function (order) {
       return {
+        _id:       order._id,
         name:      order.name,
         flavor:    order.style,
         qty:       order.qty,
@@ -30,7 +32,17 @@ router.post('/order', function (req, res) {
   collection.save(req.body, function () {
     res.redirect('/chickennuggets')
   });
+});
 
+router.post('/order/:id/complete', function (req, res) {
+  var collection = global.db.collection('chickenNuggets');
+
+  collection.update(
+    {_id: ObjectID(req.params.id)},
+    {$set: {complete: true}},
+    function () {
+      res.redirect('/chickennuggets')
+    });
 });
 
 module.exports = router;
