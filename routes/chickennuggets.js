b592489ab1d1caf4d000910285c5c3d399f2e5a0
Scott Humphries <scott@sscotth.io>
2015-08-12 12:03:32 -0500

    object oriented models

--- routes/chickennuggets.js
+++ routes/chickennuggets.js
@@ -1,25 +1,23 @@
 var express = require('express');
 var moment = require('moment');
-var ObjectID = require('mongodb').ObjectID;
+
+var Order = require('../models/ChickenNuggets');
 
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  var collection = global.db.collection('chickenNuggets');
-
-  collection.find().toArray(function (err, orders) {
-    var formattedOrders = orders.map(function (order) {
-      return {
-        _id:       order._id,
-        name:      order.name,
-        flavor:    order.style,
-        qty:       order.qty,
-        createdAt: moment(order._id.getTimestamp()).fromNow()
-      };
-    });
-
-    res.render('templates/chicken-index', {orders: formattedOrders});
+  Order.findAll(function (err, orders) {
+    res.render('templates/chicken-index', {orders: formatAllOrders(orders)});
   });
+
+  function formatAllOrders(orders) {
+    return orders.map(function (order) {
+      order.flavor = order.style;
+      order.createdAt = moment(order._id.getTimestamp()).fromNow();
+      delete order.style;
+      return order;
+    });
+  }
 });
 
 router.get('/order', function (req, res) {
@@ -27,22 +25,19 @@ router.get('/order', function (req, res) {
 });
 
 router.post('/order', function (req, res) {
-  var collection = global.db.collection('chickenNuggets');
+  var order = new Order(req.body);
 
-  collection.save(req.body, function () {
-    res.redirect('/chickennuggets')
+  order.save(function () {
+    res.redirect('/chickennuggets');
   });
 });
 
 router.post('/order/:id/complete', function (req, res) {
-  var collection = global.db.collection('chickenNuggets');
-
-  collection.update(
-    {_id: ObjectID(req.params.id)},
-    {$set: {complete: true}},
-    function () {
-      res.redirect('/chickennuggets')
+  Order.findById(req.params.id, function (err, order) {
+    order.complete(function () {
+      res.redirect('/chickennuggets');
     });
+  });
 });
 
 module.exports = router;
